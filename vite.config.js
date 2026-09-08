9f3c724bfbfa1e8286201e07a06a41ae81c43994
Vina-13dev <marcosgalo13@gmail.com>
2026-09-08 09:24:06 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Troque 'estoque-equipamentos' pelo nome exato do seu repositório no GitHub
-// se for publicar em https://SEU_USUARIO.github.io/NOME_DO_REPO/
 export default defineConfig({
   plugins: [react()],
-  base: '/estoque-equipamentos/',
+  base: '/controle-estoque-equipamentos/',
 })
